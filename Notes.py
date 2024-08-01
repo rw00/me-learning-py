703be7b20a3dc4a1f52ddc2bc4b5f7ea0330dece
RW <raafatwahb@gmail.com>
2024-08-01 10:27:51 +0200

    Create Notes.py

---
+++ Notes.py
@@ -0,0 +1,9 @@
+def funct():
+    pass # this is a placeholder
+
+class 2DPoint():
+    __slots__ = ("x", "y") # more efficient in terms of memory space and speed of access
+    
+    def __init__(self):
+        self.x = 0
+        self.y = 0
